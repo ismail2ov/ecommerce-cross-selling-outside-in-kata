9c4b381cd1a052ad8f1e7db643f55bf35006cf7a
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-10 06:51:25 +0100

    Implement Product entity

--- src/main/java/com/github/ismail2ov/ecommercecrosssellingoutsideinkata/domain/Product.java
+++ src/main/java/com/github/ismail2ov/ecommercecrosssellingoutsideinkata/domain/Product.java
@@ -1,4 +1,22 @@
 package com.github.ismail2ov.ecommercecrosssellingoutsideinkata.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String price;
 }
