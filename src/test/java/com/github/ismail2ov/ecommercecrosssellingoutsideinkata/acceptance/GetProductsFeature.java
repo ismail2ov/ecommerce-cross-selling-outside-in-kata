adbe1d059898f7bd9a25bc89cd601d39edec2974
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-10 06:36:42 +0100

    Use Testcontainers

--- src/test/java/com/github/ismail2ov/ecommercecrosssellingoutsideinkata/acceptance/GetProductsFeature.java
+++ src/test/java/com/github/ismail2ov/ecommercecrosssellingoutsideinkata/acceptance/GetProductsFeature.java
@@ -7,11 +7,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GetProductsFeature {
+
+    static final PostgreSQLContainer postgreSQLContainer;
+
+    static {
+        postgreSQLContainer = (PostgreSQLContainer) new PostgreSQLContainer("postgres:14")
+                .withUsername("james")
+                .withPassword("bond")
+                .withDatabaseName("ecommerce")
+                .withReuse(true);
+
+        postgreSQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    }
 
     @Autowired
     public TestRestTemplate testRestTemplate;
@@ -21,5 +43,6 @@ public class GetProductsFeature {
         ResponseEntity<Product[]> result = testRestTemplate.getForEntity("/api/products", Product[].class);
 
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody()).hasSize(0);
     }
 }
