ea1899d4a3f08a8327d28ddaa84f75a39ae75409
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-10 05:42:44 +0100

    Create the project

---
+++ src/main/java/com/github/ismail2ov/ecommercecrosssellingoutsideinkata/EcommerceCrossSellingOutsideInKataApplication.java
@@ -0,0 +1,13 @@
+package com.github.ismail2ov.ecommercecrosssellingoutsideinkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCrossSellingOutsideInKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCrossSellingOutsideInKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ismail2ov/ecommercecrosssellingoutsideinkata/EcommerceCrossSellingOutsideInKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ismail2ov.ecommercecrosssellingoutsideinkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCrossSellingOutsideInKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
